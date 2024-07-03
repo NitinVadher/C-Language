4aac8bd7449430b6cf0e1058ba94b7c05b7eed9f
NitinVadher <nitinvadher963@gmail.com>
2024-07-03 15:20:30 +0530

    C

---
+++ Day-1/C Start/Q1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    float ans;
+    float pi = 3.14;
+    int r;
+    printf("Enter r");
+    scanf("%d", &r);
+
+    ans = pi * r * r;
+    printf("ares of circul =%.2f", ans);
+}
---
+++ Day-2/switch.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ int main(){
+
+int a,b,c;
+
+printf("--------------------------------------------\n");
+
+printf("press 1 for Engish");
+printf("press 1 for Hindi");
+printf("press 1 for Gujarati");
+
+printf("--------------------------------------------\n");
+
+printf(" Enter your choies:");
+
+scanf("%d",a);
+
+
+
+ }
